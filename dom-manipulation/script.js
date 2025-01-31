c506a763604e2dc861fdbddc91b07a4005bcab69
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-01-31 21:00:51 +0100

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,10 +2,11 @@
 const quoteDisplay = document.getElementById('quoteDisplay');
 const newQuoteButton = document.getElementById('newQuote');
 
- ["innerHTML"]
+["innerHTML"]
 ["createAddQuoteForm"]
 ["createElement", "appendChild"]
 ["map"]
+["fetchQuotesFromServer"]
 
 let quotes = [
     { text: "The only way to do great work is to love what you do.", category: "Inspiration" },
